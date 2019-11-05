6a2fe1f2ae1edcc564f7bdbb23e0d3458a57885b
Barbara <muesingb@gmail.com>
2019-11-05 17:29:57 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |t|
+Artist.create(name: Faker::Artist.name, bio: Faker::GreekPhilosophers.quote)
+end
+
+10.times do |t|
+    Genre.create(name: Faker::Music.genre)
+end
+
+10.times do |t|
+    Song.create(name: Faker::Music::GratefulDead.song, artist_id: rand(1..10), genre_id: rand(1..10))
+end
